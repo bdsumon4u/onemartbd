91ee28c55f29a582f58339729446099772ee4779
Sumon Ahmed <bdsumon4u@gmail.com>
2026-01-10 14:20:18 +0600

    feat: update PHPUnit configuration for coverage and upgrade to Laravel 10
    - Changed PHPUnit configuration to use the new coverage format.
    - Updated Rector configuration to target Laravel 10.
    - Added PreventRequestsDuringMaintenance middleware for handling maintenance mode exceptions.

--- rector.php
+++ rector.php
@@ -21,7 +21,7 @@ return RectorConfig::configure()
     ->withSetProviders(LaravelSetProvider::class)
     ->withComposerBased()
     ->withSets([
-        LaravelLevelSetList::UP_TO_LARAVEL_90,
+        LaravelLevelSetList::UP_TO_LARAVEL_100,
         LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
         LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
         LaravelSetList::LARAVEL_CODE_QUALITY,

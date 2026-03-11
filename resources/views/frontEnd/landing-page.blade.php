eba93c2954acef2c89d642bc84f8d4525c890172
Sumon Ahmed <bdsumon4u@gmail.com>
2026-03-11 23:54:50 +0600

    fix(landing-page): update title to use product name instead of landing page title

--- resources/views/frontEnd/landing-page.blade.php
+++ resources/views/frontEnd/landing-page.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ $landingPage->title }} - {{ config('app.name') }}</title>
+    <title>{{ $landingPage->product->name }} - {{ config('app.name') }}</title>
 
     <link rel="shortcut icon"
         href="{{ $web_settings->get_fav ? asset($web_settings->get_fav->file_url) : asset('frontEnd/images/no_image.png') }}">
